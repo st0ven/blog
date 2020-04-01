1a92c3592db76f43df4c4f35d12ef6cb95e2c101
Seator, Stephen <stephen_seator@comcast.com>
2020-04-01 06:18:26 -0400

    create new branch for automated deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
+  pathPrefix: "/uxblog",
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
