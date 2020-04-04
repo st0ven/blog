8ab4c40c9c22358214d53aeeca18da9a8c26c82e
st0ven <steve.seator@gmail.com>
2020-04-04 09:50:49 -0400

    Article enhancements (#2)
    * formatting fix
    
    * back to top function and better styling for legibility
    
    Co-authored-by: Seator, Stephen <stephen_seator@comcast.com>

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -6,7 +6,7 @@ import { navigate } from "@reach/router"
 import { RichText } from "prismic-reactjs"
 import { Blockquote } from "~components/blockquote"
 import { PublishedDate } from "~components/published-date"
-import { ArrowLeftCircle } from "react-feather"
+import { ArrowLeftCircle, ArrowUpCircle } from "react-feather"
 import { getFormalDateFromString } from "~resources/chronology"
 
 import styles from "~templates/blog-post.module.scss"
@@ -19,9 +19,11 @@ function renderBodyContent(body: Array<any>) {
         return <RichText render={slice.primary.rich_text} key={key} />
         break
       case "section_title":
-        return <h3 className={styles.sectionTitle} key={key}>
-          {RichText.asText(slice.primary.section_title)}
-        </h3>
+        return (
+          <h3 className={styles.sectionTitle} key={key}>
+            {RichText.asText(slice.primary.section_title)}
+          </h3>
+        )
         break
       case "blockquote":
         return <Blockquote text={slice.primary.text} key={key} />
@@ -33,17 +35,25 @@ function renderBodyContent(body: Array<any>) {
   })
 }
 
+function scrollToTop(){
+  if(window){
+    window.scrollTo(0,0);
+  }
+}
+
 export default ({ data, pageContext }: any) => {
   const { node }: any = data.prismic.allBlog_articles.edges[0]
   const dateString: string = getFormalDateFromString(node.authored_date)
   return (
     <Layout>
-      <article className={styles.article}>
-        <Link className={styles.returnLink} to="/">
+      <header>
+        <Link className={styles.articleNavLink} to="/">
           <ArrowLeftCircle className={styles.icon} />
           Other articles
         </Link>
-        <hr className={styles.horizontalRule} />
+      </header>
+      <hr className={styles.horizontalRule} />
+      <article className={styles.article}>
         <header className={styles.header}>
           <h1 className={styles.title}>{RichText.asText(node.title)}</h1>
           <h2 className={styles.subtitle}>{RichText.asText(node.subtitle)}</h2>
@@ -54,6 +64,13 @@ export default ({ data, pageContext }: any) => {
         <hr className={styles.horizontalRule} />
         <section>{renderBodyContent(node.body)}</section>
       </article>
+      <hr className={styles.horizontalRule} />
+      <footer>
+        <a className={styles.articleNavLink} onClick={scrollToTop}>
+          <ArrowUpCircle className={styles.icon} />
+          Back to top
+        </a>
+      </footer>
     </Layout>
   )
 }
