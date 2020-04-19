f8e61d1ebee2313da21176bdc22a4ae39cc79b1f
Seator, Stephen <stephen_seator@comcast.com>
2020-04-19 07:44:52 -0400

    update to config for repo rename

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
-  pathPrefix: "/uxblog",
+  pathPrefix: "/blog",
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
